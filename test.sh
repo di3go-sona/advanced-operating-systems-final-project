743a0ea1b922db42d3eeebb48b4b8a63e2cc6ec1
di3go-sona <diego.sonaglia@gmail.com>
2021-06-17 10:32:47 +0200

    multiple threads to normal test

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
-./test send --repeat=1000 --delay=10 &
-./test recv --repeat=1000 --delay=10 &
-./test set_delay 10 --repeat=10 --delay=1000 &
-./test revoke --repeat=10 --delay=1000 &
-./test flush --repeat=10 --delay=1000 &
+./test send --repeat=100 --delay=10 --group=2 &
+./test send --repeat=100 --delay=10 --group=2 &
+./test send --repeat=100 --delay=10 --group=2 &
+./test send --repeat=100 --delay=10 --group=2 &
+
+./test recv --repeat=100 --delay=10 --group=2 &
+./test recv --repeat=100 --delay=10 --group=2 &
+./test recv --repeat=100 --delay=10 --group=2 &
+./test recv --repeat=100 --delay=10 --group=2 &
+
+./test set_delay 10 --repeat=10 --delay=1000 --group=2 &
+./test set_delay 0 --repeat=10 --delay=1000 --group=2 &
+./test revoke --repeat=10 --delay=1000 --group=2 &
+./test flush --repeat=10 --delay=1000 --group=2 &
 
